2fe15add43414a2a3a08c200d3e1ff851ea54e0f
Pro-Code-art <jayme.coder@gmail.com>
2021-01-31 17:38:47 -0800

    set up api file paths, required database, and fixed notes.html route

--- server.js
+++ server.js
@@ -7,34 +7,50 @@ const app = express();
 // Define port
 const PORT = process.env.PORT || 3000;
 
+// Require path
 const path = require('path');
+
+// Require database
+const database = require('./db/db.json');
 
 // Middleware to parse JSON
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(bodyParser.json());
   
 
-app.post("/", function(req, res){
-  let firstName = req.body.fNAME;
-  
-});
-
-// html routes (routes that will serve websites)
+// Html routes 
 // Landing page route (how to pack all the js and css dependencies into html hint: import)
-app.use( function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join( __dirname + '/public/index.html' ) );
 });
 
-// Note page route
+// Note page
 app.get('/notes', function (req, res) {
-    res.sendFile(path.join( __dirname + 'public/notes.html' ) );
+    res.sendFile(path.join( __dirname + '/public/notes.html' ) );
+});
+
+app.use(function(req, res){
+    res.setHeader('Content-Type', 'text/plain')
+    res.write('you posted:\n')
+    res.end(JSON.stringify(req.body, null, 2))
 });
 
 // API routes
+app.get('/api/notes', function(req, res) {
+    res.json(database);
+
+});
+
 
 // post route to save note
     // ADD the new note to the db.son (figure out how to parse)
+    app.post("/", function(req, res){
+        let firstName = req.body.fNAME;
+        
+      });
 
 // get route to get all notes
 
